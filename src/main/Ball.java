c6a6a5e5e4ef06d3dd9f93787a6c592620bdb457
Caque Lira <caiqueportolira@gmail.com>
2015-05-03 21:23:41 -0300

    ball

--- src/main/Ball.java
+++ src/main/Ball.java
@@ -3,14 +3,41 @@ package main;
 import engine.Game;
 import javafx.scene.canvas.*;
 
+import java.awt.Color;
+import java.util.Random;
+
 public class Ball {
-	private double speedx, speedy;
-	private double posx, posy;
-	GraphicsContext context;
+	private float velocidadeX, velocidadeY;
+	private float x, y;
+	private float largura, altura;
+	GraphicsContext gc;
 	public Ball (Game game) {
-		this.game = game;
-		context = game.getGraphicsContext();
-		posx = game.getW();
-		posy = game.getH();
+		gc = game.getGraphicsContext();
+		largura = game.getW();
+		altura = game.getH();
+		Random random = new Random (System.currentTimeMillis());
+		velocidadeX = random.nextInt(5);
+		velocidadeY = random.nextInt(5);
 	}
+	public void update() {
+        // A bolinha terá sempre a sua posição atualizada
+        x += velocidadeX;
+        y += velocidadeY;
+        // Verificamos se a posição da bolinha está saindo da tela
+        if (x > largura || x < 0) {
+            velocidadeX *= -1;
+        }
+        if (y > altura || y < 0) {
+            velocidadeY *= -1;
+        }
+    }
+	public void display() {
+        // primeiro desenhos o fundo com a cor cinza claro
+        gc.setFill(Color.LIGHTGRAY);
+        gc.fillRect(0, 0, largura, altura);
+        // vamos fazer da cor vermelha
+        gc.setFill(Color.RED);
+        // enfim, desenhamos a bolinha com a posição atualizada e o tamanho de 10
+        gc.fillOval(x, y, 30, 30);
+    }
 }
